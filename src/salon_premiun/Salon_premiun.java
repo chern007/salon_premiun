480afebe64069c45638cc264bab38edf882ab780
chern007 <carlos.hernandez_crespo@nokia>
2017-11-13 08:54:31 +0100

    commit lunes en el curro

--- src/salon_premiun/Salon_premiun.java
+++ src/salon_premiun/Salon_premiun.java
@@ -15,7 +15,23 @@ public class Salon_premiun {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new salon_premiun_form().setVisible(true);
+            }
+        });
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
     
 }
